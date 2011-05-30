89dfded0c942fe750fdfbced6fbc1308cc38d0cf
ianwookim <ianwookim@gmail.com>
2011-05-30 01:05:30 +0000

    copy command.tex to working directory.

--- src/cvmaker.hs
+++ src/cvmaker.hs
@@ -5,10 +5,22 @@ import Text.StringTemplate.Helpers
 
 import System.FilePath
 import System.IO
+import System.Directory
 
 import Paths_cvmaker
 
+workdir = "/Users/wavewave/mac/prog/cvmaker/working"
+
+
 main = do 
+  datadir <- getDataDir
+  let templdir = datadir </> "template" 
   putStrLn " cvmaker version 0.0.0.0 " 
+  putStrLn $ " templdir = " ++ templdir
+  
+  putStrLn $ " copying command.tex to " ++ workdir 
+
+  copyFile (templdir </> "command.tex") (workdir </> "command.tex")
+
 
 
