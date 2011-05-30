fc61429c1e103965de066aa3850a309fcaacce85
ianwookim <ianwookim@gmail.com>
2011-05-30 00:32:07 +0000

    cv-maker start

---
+++ src/cv-maker.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import Text.StringTemplate
+import Text.StringTemplate.Helpers
+
+import System.FilePath
+import System.IO
+
+main = do 
+  putStrLn " cv-maker version 0.0.0.0 " 
+
+
